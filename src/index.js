e73de4699cc5848f8d081198cd550ef3e7831a1c
KorotkayaNastuha <skiteikina11@gmail.com>
2022-11-04 19:22:55 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+
+function onInput(event) {
+    event.preventDefault();
+    const name = input.value.trim()
+    if (name === '') {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        return;
+    }
+}
+
+// function onError() { 
+//     Notiflix.Notify.failure('Oops, there is no country with that name.'); 
+// }
