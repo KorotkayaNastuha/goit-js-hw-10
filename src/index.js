98638ca22473fef5aa1b625947c96d3b2bb01d23
KorotkayaNastuha <skiteikina11@gmail.com>
2022-11-04 19:42:00 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 
