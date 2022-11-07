150d4f29a82bb63f7ba2833d98cca0c1961ab049
KorotkayaNastuha <skiteikina11@gmail.com>
2022-11-07 17:21:49 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries";
+import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 
@@ -8,11 +8,11 @@ const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
     event.preventDefault();
-    const name = input.value.trim()
+    const name = input.value.trim();
     if (name === '') {
         countryList.innerHTML = '';
         countryInfo.innerHTML = '';
@@ -27,16 +27,16 @@ function onInput(event) {
 function choiceCountry(countries) {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
-    if (countries.lenght > 10) {
+    if (countries.length > 10) {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    } if (countries.lenght >= 2 && countries.lenght <= 10) {
+    } if (countries.length > 2 && countries.length < 10) {
         const list = countries.map(({ flags, name }) => {
-             return `<li class="country-list__item"><img src="${flags.svg}" alt="" width="50" height="50"></li>`
+             return `<li class="country-list__item"><img src="${flags.svg}" alt="" width="50" height="50"><h2>${name.official}</h2></li>`
         }).join('');
       countryList.innerHTML = list;
-     } if (countries.lenght === 1) {
+     } if (countries.length === 1) {
         const markup = countries.map(({ flags, name, capital, population, languages }) => {
-            return `<div><img src="${flags.svg}" alt = "${name.official}" width="70" height="50"><h2>${name.official}</h2>
+            return `<div><img src="${flags.svg}" alt="${name.official}" width="70" height="50"><h2>${name.official}</h2>
             <p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${Object.values(languages)}</p></div>`
         }).join('');
         countryInfo.innerHTML = markup;
@@ -45,3 +45,5 @@ function choiceCountry(countries) {
 function onError() { 
     Notiflix.Notify.failure('Oops, there is no country with that name.'); 
 }
+
+
